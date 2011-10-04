3e29aeecd87b6a313625b3632b12f912877234b7
icarus <icarus@icarusmatoMacBook-Pro.local>
2011-10-04 10:47:45 +0800

    Initial Commit

---
+++ bwt/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bwt
+//
+//  Created by icarus on 11-10-4.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
