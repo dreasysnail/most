8f4d094598add00ba8c835eff341c9720da5b696
zyz1989 <jeremy071242044@gmail.com>
2012-05-18 20:38:26 +0800

    count almost finished

---
+++ bwt/bwt.h
@@ -0,0 +1,201 @@
+//
+//  bwt.h
+//  bwt
+//
+//  Created by zhang yizhe on 12-5-15.
+//  Copyright (c) 2012年 SJTU. All rights reserved.
+//
+
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::setw;
+using std::flush;
+using std::endl;
+using std::string;
+using std::getline;
+using std::istream;
+using std::ostream;
+using std::min;
+
+
+//
+// The maximum input string length this program
+// will handle is defined here.  A suffix tree
+// can have as many as 2N edges/nodes.  The edges
+// are stored in a hash table, whose size is also
+// defined here.
+//
+const int MAX_LENGTH = 1000;
+const int HASH_TABLE_SIZE = 2179;  //A prime roughly 10% larger
+
+
+
+//
+// When a new tree is added to the table, we step
+// through all the currently defined suffixes from
+// the active point to the end point.  This structure
+// defines a Suffix by its final character.
+// In the canonical representation, we define that last
+// character by starting at a node in the tree, and
+// following a string of characters, represented by
+// first_char_index and last_char_index.  The two indices
+// point into the input string.  Note that if a suffix
+// ends at a node, there are no additional characters
+// needed to characterize its last character position.
+// When this is the case, we say the node is Explicit,
+// and set first_char_index > last_char_index to flag
+// that.
+// 
+// from first_c_i to last_c_i :current implicit one expect to be inserted.
+
+class Suffix {
+    public :
+    int origin_node;
+    int first_char_index;
+    int last_char_index;
+    Suffix( int node, int start, int stop )
+    : origin_node( node ),
+    first_char_index( start ),
+    last_char_index( stop ){};
+    int Explicit(){ return first_char_index > last_char_index; }
+    int Implicit(){ return last_char_index >= first_char_index; }
+    void Canonize();
+    
+    //my custom function
+    int countString(const string &query );
+    bool isExistString(const string &query);
+    
+};
+
+//
+// The suffix tree is made up of edges connecting nodes.
+// Each edge represents a string of characters starting
+// at first_char_index and ending at last_char_index.
+// Edges can be inserted and removed from a hash table,
+// based on the Hash() function defined here.  The hash
+// table indicates an unused slot by setting the
+// start_node value to -1.
+//
+
+class Edge {
+    public :
+    int first_char_index;
+    int last_char_index;
+    int end_node;
+    int start_node;
+    void Insert();
+    void Remove();
+    Edge();
+    Edge( int init_first_char_index,
+         int init_last_char_index,
+         int parent_node );
+    int SplitEdge( Suffix &s );
+    static Edge Find( int node, int c );
+    static int Hash( int node, int c );
+};
+
+//
+//  The only information contained in a node is the
+//  suffix link. Each suffix in the tree that ends
+//  at a particular node can find the next smaller suffix
+//  by following the suffix_node link to a new node.  Nodes
+//  are stored in a simple array.
+//
+class Node {
+    public :
+    int suffix_node;
+    int father;
+    int leaf_index;
+
+    Node() { suffix_node = -1;
+        father=-1;
+        leaf_index=-1;}
+    void showNodeString();
+    static int Count;
+    static int Leaf;
+    //my custome var
+    //int my_node_index;
+    int leaf_count_beneath;
+    int first_char_index;
+    int last_char_index;
+    int above_edge_first_char_index;
+};
+
+
+// The Buffer class exists purely to overload operator[],
+// which allows me to return 256 for T[ N ].  Note also
+// that operator[] doesn't exactly return an int or a char
+// like you might think.  Instead, it returns an Aux object,
+// which is just really a wrapper around an integer.  This
+// lets me write an operator<<() for Aux and ostream so that
+// outputting 256 will actually print "<EOF>"
+//
+
+//
+// Since Aux is just a wrapper around an integer, all I
+// need is a holder for the integer, a constructor,
+// and a casting operator.
+//
+
+class Aux {
+    public :
+    int i;
+    Aux( int rhs ){ i = rhs; }
+    operator int(){ return i; }
+};
+
+
+class Buffer {
+    public :
+    char data[ MAX_LENGTH ];
+    int N;
+    Aux operator[]( int size ) const;
+};
+
+inline Aux Buffer::operator[]( int i ) const
+{
+    if ( i >= N )
+        return Aux( 256 );
+    else
+        return Aux( data[ i ] );
+}
+
+
+
+
+
+//
+// Necessary forward references
+//
+void validate();
+int walk_tree( int start_node, int last_char_so_far );
+
+
+//
+// The default ctor for Edge just sets start_node
+// to the invalid value.  This is done to guarantee
+// that the hash table is initially filled with unused
+// edges.
+//
+
+
+//
+void dump_edges( int current_n );
+void AddPrefix( Suffix &active, int last_char_index );
+ostream &operator<<( ostream &s, const Suffix &str );
+ostream &operator<<( ostream &s, Aux &a );
+ostream &operator<<( ostream &s, const Edge &edge );
+istream &operator>>( istream &s, Buffer &b );
+void dump_edges( Suffix s1 );
+void AddSuffixLink( int &last_parent, int parent );
--- bwt/main.cpp
+++ bwt/main.cpp
@@ -7,22 +7,79 @@
 //
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include "bwt.h"
+
 using namespace std;
 
+//global var's declarision
+extern Edge Edges[ HASH_TABLE_SIZE ];
+extern Node Nodes[ MAX_LENGTH * 2 ];
+extern char T[ MAX_LENGTH ];
+extern int N;
+
 int main()
-{	string sraw,sout;
-	int b;
-	while(cin>>sraw){
-        sout=encode(sraw);
-        cout<<"encode:"<<sout<<endl;
-    }
-	cin>>b;
+{
     
-}
+    cout << "Normally, suffix trees require that the last\n"
+    << "character in the input string be unique.  If\n"
+    << "you don't do this, your tree will contain\n"
+    << "suffixes that don't end in leaf nodes.  This is\n"
+    << "often a useful requirement. You can build a tree\n"
+    << "in this program without meeting this requirement,\n"
+    << "but the validation code will flag it as being an\n"
+    << "invalid tree\n\n";
+    
+    
+    Suffix active( 0, 0, -1 );  // The initial active prefix
+    
+    while (true) {
+        cout << "Enter string: " << flush;
+        cin.getline( T, MAX_LENGTH - 1 );
+        if (T[0]=='.')
+            break;
+        else {
+            N = strlen( T ) - 1;
+            //
+            // The active point is the first non-leaf suffix in the
+            // tree.  We start by setting this to be the empty string
+            // at node 0.  The AddPrefix() function will update this
+            // value after every new prefix is added.
+            // suffix is a tree
+            //
+            
+            for ( int i = 0 ; i <= N ; i++ )
+                AddPrefix( active, i );
+            
+            
+            
+            // leaf index means it's a leaf and its no. 
+            // Once all N prefixes have been added, the resulting table
+            // of edges is printed out, and a validation step is
+            // optionally performed.
+            //
+            //   dump_edges( N );
+            //   cout << "Would you like to validate the tree?"
+            //        << flush;
+            //   std::string s;
+            //    std::getline( cin, s ); 
+            //   if ( s.size() > 0 && s[ 0 ] == 'Y' || s[ 0 ] == 'y' )
+            //       validate();
 
-
-
+        }
+    }
+    //print info
+    for(int i=0;i<Node::Count;i++){
+        cout<<i<<"   "<<Nodes[i].father<<"   "<<Nodes[i].leaf_index<<"   "<<Nodes[i].first_char_index<<"   "<<Nodes[i].last_char_index<<"  "<<Nodes[i].leaf_count_beneath<<endl;
+        //  Nodes[i].showNodeString();
+        
+        
+        
+        
+        
+    }
+    
+    cout<<active.countString("hah")<<endl;
+   
+       return 1;
+};
 
